b70a6f916f2800b2695c64d596cbf96c9d57568e
Frank Wohlfarth <frank.wohlfarth@googlemail.com>
2026-09-05 12:58:14 +0200

    migration new

--- UI_Extended/UI/index.tsx
+++
@@ -1,8 +0,0 @@
-import { ModRegistrar } from "cs2/modding";
-import { CityMonitorComponent } from "mods/city-monitor";
-
-const register: ModRegistrar = (moduleRegistry) => {
-    moduleRegistry.append('GameTopRight', CityMonitorComponent);
-}
-
-export default register;
